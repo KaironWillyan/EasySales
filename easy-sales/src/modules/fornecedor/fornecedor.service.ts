b9eed0dfb29c088e4e26f6d1e509864ddd3ad94a
Maria Vitória S.G <mariavitoriasobrinhogoes@gmail.com>
2023-04-18 16:21:24 -0300

    Adicionando CRUD de Fornecedor

--- easy-sales/src/modules/fornecedor/fornecedor.service.ts
+++ easy-sales/src/modules/fornecedor/fornecedor.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateFornecedorDto } from './dto/create-fornecedor.dto';
 import { UpdateFornecedorDto } from './dto/update-fornecedor.dto';
+import { PrismaService } from 'src/infra/database/prisma/prisma.service';
+import { Fornecedor } from './entities/fornecedor.entity';
 
 @Injectable()
 export class FornecedorService {
-  create(createFornecedorDto: CreateFornecedorDto) {
-    return 'This action adds a new fornecedor';
+
+  constructor(private prisma: PrismaService){};
+
+  async create(data: CreateFornecedorDto):Promise<Fornecedor> {
+    const fornecedorExist = await this.prisma.fornecedor.findFirst({
+      where: {
+        id: data.id_fornecedor
+      }
+    })
+
+    if(fornecedorExist){
+      throw new BadRequestException('Fornecedor já foi cadastrado.')
+    }
+
+    const fornecedor = await this.prisma.fornecedor.create({
+      data: {
+        ...data
+      }
+    })
+
+    return fornecedor;
   }
 
   findAll() {
