49d39baf8259a92fc4e990dc192cdf8bcf0e790f
Maria Vitória S.G <mariavitoriasobrinhogoes@gmail.com>
2023-04-18 16:21:24 -0300

    Adicionando CRUD de Fornecedor

--- easy-sales/src/modules/fornecedor/entities/fornecedor.entity.ts
+++ easy-sales/src/modules/fornecedor/entities/fornecedor.entity.ts
@@ -1,1 +1,13 @@
-export class Fornecedor {}
+import { Produto } from "src/modules/produto/entities/produto.entity"
+
+export class Fornecedor {
+    id_fornecedor             :string
+    produtos                  :Produto[]
+    nome_fornecedor           :string
+    rua_fornecedor            :string
+    bairro_fornecedor         :string
+    num_fornecedor            :string
+    logradouro_fornecedor     :string
+    cep_fornecedor            :string
+    cidade_fornecedor         :string
+}
