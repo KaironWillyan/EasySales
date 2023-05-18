713c3087cbb61b8171e2a12dea012dbac4123473
JoaoFilho33 <joaod.almeidafi@gmail.com>
2023-05-17 23:31:43 -0300

    adicionando alterações

--- easy-sales/src/modules/cliente/entities/cliente.entity.ts
+++ easy-sales/src/modules/cliente/entities/cliente.entity.ts
@@ -1,1 +1,14 @@
-export class Cliente {}
+import { Prisma } from '@prisma/client';
+
+export class Cliente {
+    id_cli: string
+    nome_cli: string
+    cpf: string
+    rua_cli: string
+    bairro_cli: string
+    num_cli: string
+    logradouro_cli: string
+    cep_string: string
+    cidade_CLI: string
+    compras?: 
+}
