4ec20a957ad669b7aaa6cd7ca99dcdf0cf5e3462
kairon <kairon@kairon.kairon>
2023-06-27 23:07:49 -0300

    cors

--- easysales/src/main/java/com/easysales/EasysalesApplication.java
+++ easysales/src/main/java/com/easysales/EasysalesApplication.java
@@ -2,6 +2,9 @@ package com.easysales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -11,5 +14,17 @@ public class EasysalesApplication {
 		SpringApplication.run(EasysalesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+//				WebMvcConfigurer.super.addCorsMappings(registry);
+				registry.addMapping("/**")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 
 }
